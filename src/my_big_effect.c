cd795748470fa2176a894e7b6759d53b8e39caec
EwenBriand <ewen.briand@epitech.eu>
2022-05-04 16:59:54 +0200

    [FIX] particul effect

--- src/my_big_effect.c
+++ src/my_big_effect.c
@@ -26,30 +26,8 @@ static void draw_particule(sfRenderWindow *window, game_t *game)
     sfRenderWindow_display(window);
 }
 
-static void change_pos(game_t *game, int i, int sec)
+static void reset_particule(game_t *game)
 {
-    srand(time(NULL));
-    int speed = (rand() % 30 + 1) / 10;
-    new_pos(
-        sfVertexArray_getVertex(game->particule_array[0], i), speed, sec, i);
-    new_pos(
-        sfVertexArray_getVertex(game->particule_array[1], i), speed, sec, i);
-    new_pos(
-        sfVertexArray_getVertex(game->particule_array[2], i), speed, sec, i);
-}
-
-void my_big_effect(sfRenderWindow *window, game_t *game)
-{
-    double sec = 0;
-
-    sfClock_restart(game->clock);
-    while (sec < 1) {
-        printf("%f\n", sec);
-        sec = sfClock_getElapsedTime(game->clock).microseconds / 1000000.0;
-        for (int i = 0; i < 360; ++i)
-            change_pos(game, i, sec);
-        draw_particule(window, game);
-    }
     for (int i = 0; i < 360; ++i) {
         sfVertexArray_getVertex(game->particule_array[0], i)->position =
             (sfVector2f){1920 / 2 + 0, 1080 / 2 + 0};
@@ -58,4 +36,27 @@ void my_big_effect(sfRenderWindow *window, game_t *game)
         sfVertexArray_getVertex(game->particule_array[2], i)->position =
             (sfVector2f){1920 / 2 - 100, 1080 / 2 - 100};
     }
+}
+
+void my_big_effect(sfRenderWindow *window, game_t *game)
+{
+    srand(time(NULL));
+    double sec = 0;
+    int speed = 0;
+
+    sfClock_restart(game->clock);
+    while (sec < 1) {
+        sec = sfClock_getElapsedTime(game->clock).microseconds / 1000000.0;
+        for (int i = 0; i < 360; ++i) {
+            speed = (rand() % 30 + 1) / 10;
+            new_pos(sfVertexArray_getVertex(game->particule_array[0], i),
+                speed, sec, i);
+            new_pos(sfVertexArray_getVertex(game->particule_array[1], i),
+                speed, sec, i);
+            new_pos(sfVertexArray_getVertex(game->particule_array[2], i),
+                speed, sec, i);
+        }
+        draw_particule(window, game);
+    }
+    reset_particule(game);
 }
